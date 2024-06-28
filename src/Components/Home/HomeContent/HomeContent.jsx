edf38a186979a00a9e8e183f8d6ed902696be814
Suleman Qureshi <143796869+Suleman-Qureshi@users.noreply.github.com>
2024-06-29 03:17:38 +0500

    size increaded

--- src/Components/Home/HomeContent/HomeContent.jsx
+++ src/Components/Home/HomeContent/HomeContent.jsx
@@ -13,14 +13,14 @@ function HomeContent({lines,image_src_2,image_src_3,image_src_4,box}) {
        <p className='text-[#ED7D31] text-6xl font-bold montserrat slide2 translate-x-[-200%]' style={{fontStyle:'italic'}}>PRODUCTION</p>
        <p className='text-2xl font-semibold text-white quicksand slide3 translate-x-[-200%]'>Your Vision, Our Digital Craft</p>
     </div>
-    <div className='relative top-10'>
+    <div className='relative top-10 right-10'>
     <div className='-translate-y-[14rem] -translate-x-36'><img src={image_src_4} className=' h-[6rem] animate-spin'/></div>
     <div className='-translate-y-[15.5rem] -translate-x-10 '><img src={image_src_4} className=' h-14 animate-spin'/></div>
-    <div className='absolute w-[25rem] h-[25rem] -top-44 right-0'>
+    <div className='absolute w-[26rem] h-[26rem] -top-44 right-0'>
     <img src={image_src_2} className=' w-full h-full'/>
     </div>
-    <img src={image_src_3} className='absolute h-10 bottom-[16rem] right-[21.2rem] animate-spin-reverse'/>
-    <img src={image_src_3} className='absolute h-10 bottom-[14.5rem] right-[19.2rem]  animate-spin'/>
+    <img src={image_src_3} className='absolute h-10 bottom-[16rem] right-[21.7rem] animate-spin-reverse'/>
+    <img src={image_src_3} className='absolute h-10 bottom-[14.5rem] right-[19.7rem]  animate-spin'/>
     </div>
     </div>
     </>
